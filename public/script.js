c9d76c078a055aaa26d7e684de9bcc776b4c8327
Benjamin <legoawesomepants@gmail.com>
2022-03-27 16:57:41 -0500

    Add DragonFrame integration

--- public/script.js
+++ public/script.js
@@ -1,10 +1,13 @@
 var socket = io();
-const image = document.getElementById("bg-image-wrapper");
+const image = document.getElementById("bg-image");
 /*fetch('/latestFrame')
 .then((res) => {
     console.log(JSON.parse(res))
     image.innerHTML = '<img id="bg-image" src="/latestFrame"/>';
 })*/
 socket.on("frameChange", () => {
-    window.location.reload(false);
+    //const timestamp = new Date().getTime();
+    console.log("frameChange");
+    //image.src="";
+    image.src=`/latestFrame?${new Date().getTime()}`;
 })
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//require our dependencies
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan');
@@ -5,6 +6,9 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
+const udp = require('dgram');
+const UDPport = 8888;
+const { Buffer } = require('buffer');
 
 
 const backgrounds = './backgrounds/';
@@ -12,6 +16,7 @@ const backgrounds = './backgrounds/';
 let folder = 'imgsqnce/';
 let neededPath;
 let images = [];
+let DFinternalFrame;
 //initializing the images
 neededPath = path.join(__dirname, path.join(backgrounds, folder));
 console.log(neededPath);
@@ -29,7 +34,7 @@ fs.readdir(neededPath, function (err, files) {
     });
 });
 console.log('Indexing Complete');
-let frameCount = 1;
+let frameCount = 0;
 // Creating express app 
 const app = express();
 const server = http.createServer(app);
@@ -39,25 +44,34 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
+// Creating UDP server
+const UDPserver = udp.createSocket('udp4');
+
 function updateFrames() {
     io.emit('frameChange',frameCount);
 }
+function advanceFrame() {
+    frameCount ++;
+    updateFrames();
+}
+function previousFrame(){
+    frameCount --;
+    updateFrames();
+}
+//Our HTTP listeners for integration with BOATS and the manual controller:
+
 app.get('/latestFrame',(req, res) => {
-    res.sendFile(path.join(neededPath,images[frameCount]));
-    
-    
+    res.sendFile(path.join(neededPath,images[frameCount % images.length]));
 })
 app.get('/connect',(req, res) => {
     res.json('Connection was successful');
 })
 app.get('/advanceFrame',(req, res) => {
-    frameCount ++;
-    updateFrames();
+    advanceFrame();
     res.json(frameCount);
 })
 app.get('/deletePreviousFrame',(req, res) => {
-    frameCount --;
-    updateFrames();
+    previousFrame();
     res.json(frameCount);
 })
 app.post('/setFrame', (req, res) => {
@@ -66,10 +80,62 @@ app.post('/setFrame', (req, res) => {
 io.on("connect", (socket) => {
     console.log(`CONNECTED ${socket}`)
 })
+
+//Our UDP listeners for integration with DragonFrame:
+
+UDPserver.on('listening',function() {
+    const UDPaddress = UDPserver.address();
+    const UDPport = UDPaddress.port;
+    console.log(`UDP server is listening at port ${UDPport}`)
+})
+
+UDPserver.on('message',function(msg,info){
+    const DFmessage = JSON.parse(msg.toString());
+    const event = DFmessage.event;
+     if(event === 'hello'){
+        UDPserver.send(Buffer.from('{"command":"hello","version":1.0}'),info.port,'localhost',function(error){
+            if(error){
+           client.close();
+            }else{
+           console.log('Data sent !');
+            }
+         });
+    } else if(event === 'captureComplete') {
+        console.log(`Shot frame ${DFmessage.frame}`);
+        DFinternalFrame = DFmessage.frame;
+        advanceFrame();
+    } else if (event === 'delete') {
+        console.log('deleted frame');
+        previousFrame();
+        
+    } else if (event === 'position'){
+        /*
+        console.log('Position Event');
+
+        const newFrame = parseFloat(DFmessage.frame);
+        console.log('newFrame: ' + newFrame);
+        console.log('DFinternalFrame: ' + DFinternalFrame);
+        if(newFrame === DFinternalFrame - 1){
+            console.log('user deleted most recent frame');
+        }
+        DFinternalFrame = parseFloat(newFrame);
+        */
+    }
+   });
+UDPserver.on('error',function(error){
+    console.log('Error: ' + error);
+    UDPserver.close();
+  });
+//UDPserver.bind(UDPport);
+
+
+
+
+
 // Port Number
 const port = 5000;
-console.log(JSON.stringify(images[1]));
 // Server setup
 server.listen(port, () => {
     console.log(`NU is listening on PORT ${port}`)
 })
+UDPserver.bind(port);
